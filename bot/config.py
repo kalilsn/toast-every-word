693133df7734f61c092a09ef635eda11e0e07874
Kalil Smith-Nuevelle <ksmithnuevelle@oomphinc.com>
2017-08-17 21:58:50 -0400

    Update repo structure

---
+++ bot/config.py
@@ -0,0 +1,12 @@
+"""
+This module contains api keys and other secrets that should not be committed to a public repository.
+"""
+
+WORDNIK_KEY = "wordnik key"
+
+TWITTER = {
+    "key": "consumer key",
+    "secret": "consumer secret",
+    "token": "access token",
+    "token_secret": "access token secret"
+}
